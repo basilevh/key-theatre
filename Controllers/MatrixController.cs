c2131144f0b4555a85ad4e87871786df491d735c
basilevh <basile@outlook.be>
2018-05-05 20:04:09 +0200

    Created stub classes for 2 types of LED controllers

---
+++ Controllers/MatrixController.cs
@@ -0,0 +1,14 @@
+﻿// Started 05-05-2018, Basile Van Hoorick
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeyDecorator.Controllers
+{
+    public class MatrixController
+    {
+    }
+}
